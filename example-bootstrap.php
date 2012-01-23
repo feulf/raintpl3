9f9749d74a60b5604d9839d617846ace8faa34b6
Federico Ulfo <rainelemental@gmail.com>
2012-01-23 06:43:26 -0500

    Template examples reorganized

---
+++ example-bootstrap.php
@@ -0,0 +1,48 @@
+<?php
+
+	// include
+	include "library/RainTpl.php";
+	
+	// configure
+	RainTpl::configure( "base_url", null );
+	RainTpl::configure( "tpl_dir", "templates/" );
+	RainTpl::configure( "cache_dir", "cache/" );
+	RainTpl::configure( "debug", true );
+	RainTpl::configure( "auto_escape", true );
+
+
+	// set variables
+	$var = array(
+					"variable"	=> "Hello World!",
+					"version"	=> "3.0 Alpha",
+					"menu"		=> array(
+											array("name" => "Home", "link" => "index.php", "selected" => true ),
+											array("name" => "FAQ", "link" => "index.php/FAQ/", "selected" => null ),
+											array("name" => "Documentation", "link" => "index.php/doc/", "selected" => null )
+										),
+					"week"		=> array( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ),
+					"user"		=> (object) array("name"=>"Rain", "citizen" => "Earth", "race" => "Human" ),
+					"numbers"	=> array( 3, 2, 1 ),
+					"bad_text"	=> 'Hey this is a malicious XSS <script>alert(1);</script>',
+					"table"		=> array( array( "Apple", "1996" ), array( "PC", "1997" ) ),
+					"title"		=> "Rain TPL 3 - Easy and Fast template engine", 
+					"copyright" => "Copyright 2006 - 2012 Rain TPL<br>Project By Rain Team",
+
+				);
+
+	// add a function
+	RainTpl::register_tag(	"({@.*?@})", // preg split
+						"{@(.*?)@}", // preg match
+						function( $params ){ // function called by the tag
+												$value = $params[0];
+												return "Translate: <b>$value</b>";
+										   } 
+					 );
+
+	// draw
+	$tpl = new RainTpl;
+	$tpl->assign( $var );
+	echo $tpl->draw( "bootstrap/hero" );
+
+        
+?>
