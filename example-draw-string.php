311cae0a207bb9107620f6bc912d3e7c27dbfb04
Rain <rainelemental@gmail.com>
2012-03-03 10:50:58 -0500

    Long string test

--- example-draw-string.php
+++ example-draw-string.php
@@ -1,10 +1,11 @@
 <?php
 
-	memory_usage_start();
-	timer_start();
-
     // namespace
     use Rain\Tpl;
+
+
+	memory_usage_start();
+	timer_start();
 
 	// include
 	include "library/Rain/Tpl.php";
@@ -20,13 +21,54 @@
 
 	Tpl::configure( $config );
 
+	// set variables
+	$var = array(
+					"variable"	=> "Hello World!",
+					"version"	=> "3.0 Alpha",
+					"menu"		=> array(
+											array("name" => "Home", "link" => "index.php", "selected" => true ),
+											array("name" => "FAQ", "link" => "index.php/FAQ/", "selected" => null ),
+											array("name" => "Documentation", "link" => "index.php/doc/", "selected" => null )
+										),
+					"week"		=> array( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ),
+					"user"		=> (object) array("name"=>"Rain", "citizen" => "Earth", "race" => "Human" ),
+					"numbers"	=> array( 3, 2, 1 ),
+					"bad_text"	=> 'Hey this is a malicious XSS <script>alert(1);</script>',
+					"table"		=> array( array( "Apple", "1996" ), array( "PC", "1997" ) ),
+					"title"		=> "Rain TPL 3 - Easy and Fast template engine",
+					"copyright" => "Copyright 2006 - 2012 Rain TPL<br>Project By Rain Team",
 
-	$var['title'] = 'Federico';
+				);
+
+	// add a tag
+	Tpl::register_tag(	"({@.*?@})", // preg split
+						"{@(.*?)@}", // preg match
+						function( $params ){ // function called by the tag
+												$value = $params[1][0];
+												return "Translate: <b>$value</b>";
+										   }
+					 );
+
+
+	// add a tag
+	Tpl::register_tag(	"({%.*?%})", // preg split
+						"{%(.*?)(?:\|(.*?))%}", // preg match
+						function( $params ){ // function called by the tag
+												$value = $params[1][0];
+                                                $value2 = $params[2][0];
+
+												return "Translate: <b>$value</b> in <b>$value2</b>";
+										   }
+					 );
+
+
+
+	$string = file_get_contents( "templates/test/test.html");
 
 	// draw
 	$tpl = new Tpl;
 	$tpl->assign( $var );
-	$tpl->draw_string( 'Hello {$title} how are you?' );
+	echo $tpl->draw_string( $string );
 
 	echo "<br>---------<br>";
 	echo memory_usage();
