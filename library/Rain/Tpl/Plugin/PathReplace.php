253cbf6ce67fab4c2f3a210138e4cf010fdb1f7a
Rain <rainelemental@gmail.com>
2012-10-14 13:23:44 -0400

    Reduce path improved

--- library/Rain/Tpl/Plugin/PathReplace.php
+++ library/Rain/Tpl/Plugin/PathReplace.php
@@ -28,11 +28,13 @@ class PathReplace extends \Rain\Tpl\Plugin
 		$template_directory = $context->conf['base_url'] . $context->conf['tpl_dir'] . $context->template_basedir;
 
 		// reduce the path
-		$path = str_replace( "://", "@not_replace@", $template_directory );
-		$path = str_replace( "//", "/", $path );
-		$path = str_replace( "@not_replace@", "://", $path );
-		$path = str_replace( "/./", "/", $path );
-		$path = preg_replace('/\w+\/\.\.\//', '', $path );
+                $path = str_replace( "://", "@not_replace@", $path );
+                $path = preg_replace( "#(/+)#", "/", $path );
+                $path = preg_replace( "#(/\./+)#", "/", $path );
+                $path = str_replace( "@not_replace@", "://", $path );
+                while( preg_match( '#\.\./#', $path ) ){
+                    $path = preg_replace('#\w+/\.\./#', '', $path );
+                }
 
 		$exp = $sub = array();
 
