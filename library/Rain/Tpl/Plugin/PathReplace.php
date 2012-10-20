70f0a934daaf7b50e33d25afd028ef5d37790259
Rain <rainelemental@gmail.com>
2012-10-20 12:37:28 -0400

    PSR and fix pathReplace

--- library/Rain/Tpl/Plugin/PathReplace.php
+++ library/Rain/Tpl/Plugin/PathReplace.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../Plugin.php';
 
 class PathReplace extends \Rain\Tpl\Plugin
 {
-	protected $hooks = array('before_parse');
+	protected $hooks = array('beforeParse');
 	private $tags = array('a', 'img', 'link', 'script', 'input');
 
 	/**
@@ -16,7 +16,7 @@ class PathReplace extends \Rain\Tpl\Plugin
 	 *
 	 * @param \ArrayAccess $context
 	 */
-	public function before_parse(\ArrayAccess $context){
+	public function beforeParse(\ArrayAccess $context){
 
 		// set variables
 		$html = $context->code;
@@ -28,13 +28,16 @@ class PathReplace extends \Rain\Tpl\Plugin
 		$template_directory = $context->conf['base_url'] . $context->conf['tpl_dir'] . $context->template_basedir;
 
 		// reduce the path
-                $path = str_replace( "://", "@not_replace@", $path );
+                $path = str_replace( "://", "@not_replace@", $template_directory );
                 $path = preg_replace( "#(/+)#", "/", $path );
                 $path = preg_replace( "#(/\./+)#", "/", $path );
                 $path = str_replace( "@not_replace@", "://", $path );
+
                 while( preg_match( '#\.\./#', $path ) ){
                     $path = preg_replace('#\w+/\.\./#', '', $path );
                 }
+                
+                
 
 		$exp = $sub = array();
 
@@ -68,7 +71,7 @@ class PathReplace extends \Rain\Tpl\Plugin
 
 
 
-	public function set_tags($tags) {
+	public function setTags($tags) {
 		$this->tags = (array) $tags;
 		return $this;
 	}
