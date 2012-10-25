c875a092b8efe24d793c9ed4eb2a9253a362b7fa
faryshta <angeldelcaos@gmail.com>
2012-10-24 19:05:23 -0500

    the exceptions use unifieds get/set methods

--- library/Rain/Tpl/SyntaxException.php
+++ library/Rain/Tpl/SyntaxException.php
@@ -22,46 +22,35 @@ class SyntaxException extends Exception {
     protected $tag = null;
 
     /**
-     * Returns line in template file where error has occured
-     * or null if line is not defined.
-     *
-     * @return int | null
+	   * Handles the line in template file
+	   * where error has occured
+     * 
+     * @param int | null $line
+     *  	
+     * @return \Rain\Tpl\SyntaxException | int | null
      */
-    public function getTemplateLine() {
-        return $this->templateLine;
-    }
+    public function templateLine($line){
+        if(is_null($line))
+            return $this->templateLine;
 
-    /**
-     * Sets  line in template file where error has occured.
-     *
-     * @param int $templateLine
-     * @return RainTpl_SyntaxException
-     */
-    public function setTemplateLine($templateLine) {
-        $this->templateLine = (int) $templateLine;
+        $this->templateLine = (int) $line;
         return $this;
     }
-
+  	
     /**
-     * Returns tag which caused an error.
+     * Handles the tag which caused an error.
      *
-     * @return string
-     */
-    public function getTag() {
-        return $this->tag;
-    }
+     * @param string | null $tag
+     *
+     * @return \Rain\Tpl_SyntaxException | string | null
+     */	  	
+    public function tag($tag=null){
+        if(is_null($tag))
+            return $this->tag;
 
-    /**
-     * Sets tag which caused an error.
-     *
-     * @param string $tag
-     * @return RainTpl_SyntaxException
-     */
-    public function setTag($tag) {
         $this->tag = (string) $tag;
         return $this;
-    }
-
+     }
 }
 
 // -- end
