636c314f797e034d5355cca5fa70cce6bc088f29
Rain <rainelemental@gmail.com>
2012-10-25 16:03:53 -0400

    Compress plugin

--- library/Rain/Tpl.php
+++ library/Rain/Tpl.php
@@ -78,6 +78,16 @@ class Tpl {
         ob_start();
         require $this->checkTemplate($templateFilePath);
         $html = ob_get_clean();
+        
+        
+        // Execute plugins, before_parse
+        $context = $this->getPlugins()->createContext(array(
+            'code' => $html,
+            'conf' => static::$conf,
+        ));
+        $this->getPlugins()->run('afterDraw', $context);
+        $html = $context->code;
+        
         if ($toString)
             return $html; 
         else
@@ -92,6 +102,16 @@ class Tpl {
         ob_start();
         require $this->checkString($string);
         $html = ob_get_clean();
+
+
+        // Execute plugins, before_parse
+        $context = $this->getPlugins()->createContext(array(
+            'code' => $html,
+            'conf' => static::$conf,
+        ));
+        $this->getPlugins()->run('afterDraw', $context);
+        $html = $context->code;
+
         if ($toString)
             return $html; 
         else
@@ -394,8 +414,8 @@ class Tpl {
                     //get the included template
                     $includeTemplate = $actualFolder . $this->varReplace($matches[1], $loopLevel);
 
-                    // reduce the path
-                    $includeTemplate = preg_replace('/\w+\/\.\.\//', '', $includeTemplate);
+                    // reduce the path                    
+                    $includeTemplate = Tpl::reducePath( $includeTemplate );
                     
                     //dynamic include
                     $parsedCode .= '<?php require $this->checkTemplate("' . $includeTemplate . '");?>';
@@ -691,6 +711,19 @@ class Tpl {
 
             return false;
         }
+    }
+    
+    public static function reducePath( $path ){
+        // reduce the path
+        $path = str_replace( "://", "@not_replace@", $path );
+        $path = preg_replace( "#(/+)#", "/", $path );
+        $path = preg_replace( "#(/\./+)#", "/", $path );
+        $path = str_replace( "@not_replace@", "://", $path );
+
+        while( preg_match( '#\.\./#', $path ) ){
+            $path = preg_replace('#\w+/\.\./#', '', $path );
+        }
+        return $path;
     }
 
 }
---
+++ templates/compress/test_compress.html
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Test</title>
+	<script src="test.js" ></script>
+	<link rel="stylesheet" href="style.css" type="text/css" ></link>
+</head>
+<body>
+    
+    {* This is a comment *}
+
+	<h1>Test Rain Tpl {$version}</h1>
+	<hr>
+
+	<h2>Variables</h2>
+	Variable: {$variable} <br><br>
+	Init Variable {$v = 10} <br><br>
+	Show Variable {$v} <br><br>
+	Modifier {$variable|strlen} <br><br>
+	Cascade Modifier {$variable|substr:2,5|strlen} <br><br>
+	Scoping (array) {$week.0} <br><br>
+	Var test {$variable} <br><br>
+
+
+	<h2>Loop</h2>
+	Simple Loop
+	<ul>
+		{loop="$week"}
+		<li>
+			{$key} {$value}
+		</li>
+		{/loop}
+	</ul><br><br>
+
+	Modifier on Loop
+	<ul>
+		{loop="$week|array_reverse" as $i}
+		<li>{$i}</li>
+		{/loop}
+	</ul><br><br>
+
+	Simple Nested Loop
+	<ul>
+		{loop="$table"}
+		<li>
+			{loop="$value"}
+			{$value},
+			{/loop}
+		</li>
+		{/loop}
+	</ul><br><br>
+
+	Loop on created array
+	<ul>
+		{loop="range(5,10)" as $i}
+		<li>{$i}</li>
+		{/loop}
+	</ul><br><br>
+
+	<h2>If</h2>
+	True condition: {if="true"}This is true{/if} <br><br>
+	Modifier inside if: {if="$variable|is_string"}True{/if} <br><br>
+
+</body>
+</html>
