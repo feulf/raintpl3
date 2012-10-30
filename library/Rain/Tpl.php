7347f35ba490f8d94ec33d283985dbc200b8ae42
Federico <rainelemental@gmail.com>
2012-10-30 14:54:34 -0400

    FluentInterface added on assign

--- library/Rain/Tpl.php
+++ library/Rain/Tpl.php
@@ -124,6 +124,8 @@ class Tpl {
             $this->var = $variable + $this->var;
         else
             $this->var[$variable] = $value;
+        // return this for FluentInterface, example new Tpl()->assign('title','name')->draw('template');
+        return $this;
     }
 
     /**
