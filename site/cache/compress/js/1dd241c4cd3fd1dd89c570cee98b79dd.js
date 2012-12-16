48b043ea015327e36af135ea8be0678b70bf1c68
Federico <rainelemental@gmail.com>
2012-12-15 22:56:45 -0500

    jekyll website update

--- site/cache/compress/js/1dd241c4cd3fd1dd89c570cee98b79dd.js
+++
@@ -1,7 +0,0 @@
-/*---
- Javascript compressed in Rain 
- templates/compress/test.js 
----*/
-
-var testClass = {  testFunction: function(){  alert('this is a test, Javascript can break easily so test your JS before use the CompressPlugin in production');  } }; testClass.testFunction();
-
